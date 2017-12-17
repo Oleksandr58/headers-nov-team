44f0dd6d8ebba48d790e9d59bd646faebb70757b
Oleksandr58 <kovaliukoleksandr@gmail.com>
2017-12-17 06:05:12 +0200

    Add fifth(5) header, create js folder, put logo.jpg in img

---
+++ app/js/header5.js
@@ -0,0 +1,44 @@
+var touches = document.querySelectorAll('.fifth__touch');
+var left = document.querySelector('.fifth__arrow--left');
+var right = document.querySelector('.fifth__arrow--right');
+var number = 0;
+
+for(var i = 0; i < touches.length; i++) {
+    touches[i].addEventListener('click', Click(i));
+}
+
+function Click(index) {
+    return function (e) {
+        number = index;
+        clearClasses();
+        this.classList.add('fifth__touch--active');
+    }
+}
+
+left.addEventListener('click', function() {
+    number--;      
+
+    if (number < 0) {
+        number = touches.length - 1;
+    }
+
+    clearClasses();
+    touches[number].classList.add('fifth__touch--active');  
+} );
+
+right.addEventListener('click', function() {
+    number++;      
+
+    if (number > touches.length - 1) {
+        number = 0;
+    }
+
+    clearClasses();
+    touches[number].classList.add('fifth__touch--active');  
+} );
+
+function clearClasses() {    
+    for(var j = 0; j < touches.length; j++) {
+        touches[j].classList.remove('fifth__touch--active');
+    }
+}
